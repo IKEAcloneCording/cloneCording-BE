6d5a77f54ea7795f91fef0b2da5c0c69b95e0b1c
송민진 <m_6595@naver.com>
2022-09-13 00:24:28 +0900

    테스트코드 추가

--- src/main/java/com/innovation/backend/dto/request/SignupRequestDto.java
+++ src/main/java/com/innovation/backend/dto/request/SignupRequestDto.java
@@ -1,9 +1,12 @@
 package com.innovation.backend.dto.request;
 
+import com.innovation.backend.entity.Member;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class SignupRequestDto {
     private String email;
@@ -11,4 +14,9 @@ public class SignupRequestDto {
     private String name;
     private String phone_number;
     private String address;
+
+    // 테스트용 메소드
+    public Member toEntity() {
+        return Member.builder().id(1L).username(email).password(password).name(name).phoneNumber(phone_number).address(address).build();
+    }
 }
