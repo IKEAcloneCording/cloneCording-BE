d2390f13c535df08065ea645ab50a688037b01c0
decaf-latte <hyejin.jeon940120@gmail.com>
2022-09-13 01:24:34 +0900

    bigdecimal 변경

--- src/main/java/com/innovation/backend/dto/response/CartResponseDto.java
+++ src/main/java/com/innovation/backend/dto/response/CartResponseDto.java
@@ -2,21 +2,22 @@ package com.innovation.backend.dto.response;
 
 import com.innovation.backend.entity.Cart;
 import com.innovation.backend.entity.Product;
+import java.math.BigDecimal;
 import lombok.Getter;
 
 @Getter
 public class CartResponseDto {
 
-  private Product product;
-  private Integer count;
-  private Integer sum;
-  private Integer delivery_fee;
+  private final Product product;
+  private final Integer count;
+  private final BigDecimal sum;
+  private final BigDecimal delivery_fee;
 
   public CartResponseDto (Cart cart){
     this.product = cart.getProduct();
     this.count = cart.getCount();
     this.sum = cart.getSum();
-    this.delivery_fee = count * sum ;
+    this.delivery_fee = new BigDecimal(count * 15000 );
 
   }
 
