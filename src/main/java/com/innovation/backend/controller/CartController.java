64fb654dbb055707c54f6315f3fc17589a4f664f
decaf-latte <hyejin.jeon940120@gmail.com>
2022-09-14 22:36:35 +0900

    Swagger 설정 #2 장바구니 swagger 설정

--- src/main/java/com/innovation/backend/controller/CartController.java
+++ src/main/java/com/innovation/backend/controller/CartController.java
@@ -10,6 +10,12 @@ import com.innovation.backend.exception.ErrorCode;
 import com.innovation.backend.repository.ProductRepository;
 import com.innovation.backend.security.user.UserDetailsImpl;
 import com.innovation.backend.service.CartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import javax.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +29,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Tag(name="[장바구니 API]")
 @Slf4j
 @RequiredArgsConstructor
 @RestController
@@ -32,6 +40,10 @@ public class CartController {
   private final ProductRepository productRepository;
 
   //장바구니 상품 추가
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "상품추가 성공", content = @Content(schema = @Schema(implementation = CartResponseDto.class))),
+      @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스 접근", content = @Content(schema = @Schema(implementation = ErrorCode.class))) })
+  @Operation(summary = "장바구니 상품 추가", description = "장바구니에 없는 상품이면 신규 추가, 기존에 존재하던 상품이면 갯수를 추가합니다.")
   @PostMapping("/api/auth/cart")
   public ResponseDto<CartResponseDto> addCart(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CartRequestDto cartRequestDto) {
     CartResponseDto cartResponseDto;
@@ -52,6 +64,10 @@ public class CartController {
 
 
   //장바구니 조회
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "상품조회 성공", content = @Content(schema = @Schema(implementation = CartGetListResponseDto.class))),
+      @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스 접근", content = @Content(schema = @Schema(implementation = ErrorCode.class))) })
+  @Operation(summary = "장바구니 조회", description = "장바구니에 넣은 모든 상품과 총 합산금액을 조회합니다.")
   @GetMapping("/api/auth/cart")
   public ResponseDto<CartGetListResponseDto> getCart(@AuthenticationPrincipal UserDetailsImpl userDetails){
     CartGetListResponseDto cartGetListResponseDto;
@@ -72,6 +88,10 @@ public class CartController {
 
 
   //장바구니 상품 수량 변경
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "상품 수량 변경 성공", content = @Content(schema = @Schema(implementation = CartResponseDto.class))),
+      @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스 접근", content = @Content(schema = @Schema(implementation = ErrorCode.class))) })
+  @Operation(summary = "장바구니 상품 수량 변경", description = "cart_id 를 통해 해당 상품의 갯수 (count)를 변경합니다.")
   @PutMapping("/api/auth/cart/{id}/change-count")
   public ResponseDto<CartResponseDto>  changeCount (@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CartRequestDto cartRequestDto ){
     CartResponseDto cartResponseDto;
@@ -91,6 +111,7 @@ public class CartController {
 
 
   //장바구니 상품 삭제
+  @Operation(summary = "장바구니 상품 하나 삭제", description = "장바구니에 있는 상품 중 하나만 삭제합니다.")
 @DeleteMapping("/api/auth/cart/{id}")
 public ResponseDto<String> deleteOneItem (@PathVariable Long id){
   try {
@@ -107,6 +128,7 @@ public ResponseDto<String> deleteOneItem (@PathVariable Long id){
 }
 
   //장바구니 상품 전체 삭제
+  @Operation(summary = "장바구니 상품 전체 삭제", description = "장바구니에 있는 모든 상품을 삭제합니다.")
   @DeleteMapping("/api/auth/cart")
   public ResponseDto<String> deleteAllItem (@AuthenticationPrincipal UserDetailsImpl userDetails){
     try {
