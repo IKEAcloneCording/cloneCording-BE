dcd52bfe7234a1a9f7c8b10b1aa106cb5805e815
송민진 <m_6595@naver.com>
2022-09-14 10:03:02 +0900

    카테고리 조회 api 변경

--- src/main/java/com/innovation/backend/controller/ProductController.java
+++ src/main/java/com/innovation/backend/controller/ProductController.java
@@ -5,10 +5,7 @@ import com.innovation.backend.entity.Product;
 import com.innovation.backend.exception.ErrorCode;
 import com.innovation.backend.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,9 +13,9 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @RequestMapping(value = "/api/products", method = RequestMethod.GET)
-    public ResponseDto<?> showProductsByCategory(@RequestParam (value="category", defaultValue = "") String category) {
-        return productService.showProductsByCategory(category);
+    @RequestMapping(value = "/api/products/cat/{categoryName}", method = RequestMethod.GET)
+    public ResponseDto<?> showProductsByCategory(@PathVariable String categoryName) {
+        return productService.showProductsByCategory(categoryName);
     }
 
     @RequestMapping(value = "/api/products/search", method = RequestMethod.GET)
